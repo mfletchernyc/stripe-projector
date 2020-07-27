7e1e2cda119717a6a48d34e031773219677fe8f2
M. Fletcher <deadnancy@cpe-74-65-203-139.nyc.res.rr.com>
2020-07-27 15:08:52 -0400

    Avoid confusing variable reuse.

--- src/app/stripeObject.js
+++ src/app/stripeObject.js
@@ -1,27 +1,26 @@
 import { randomInteger } from './randomThings'
 
 const stripeObject = (id, colors, settings) => {
-  let {
+  const {
     position,
     direction,
     opacity,
     width
   } = settings
 
-  const color = colors[randomInteger(0, colors.length - 1)]
-
-  position = randomInteger(position.min, position.max)
-  direction = randomInteger(direction.min, direction.max) / 100
-  opacity = randomInteger(opacity.low, opacity.high) / 100
-  width = randomInteger(width.low, width.high)
+  const randomColor = colors[randomInteger(0, colors.length - 1)]
+  const randomPosition = randomInteger(position.min, position.max)
+  const randomDirection = randomInteger(direction.min, direction.max) / 100
+  const randomOpacity = randomInteger(opacity.low, opacity.high) / 100
+  const randomWidth = randomInteger(width.low, width.high)
 
   return {
     id,
-    position,
-    direction,
-    color,
-    opacity,
-    width
+    color: randomColor,
+    position: randomPosition,
+    direction: randomDirection,
+    opacity: randomOpacity,
+    width: randomWidth
   }
 }
 
